4c1ae2bde4802c6829598e467c759f6ed6f72785
Amirul <Amirul@example.com>
2019-02-15 16:00:34 +0800

    welcome two.php

--- resources/views/welcometwo.blade.php
+++ resources/views/welcometwo.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel master
+                    welcome two
                 </div>
 
                 <div class="links">
