72a2e8c88bb315074a238e22ba6075fc934f331a
Amirul <Amirul@example.com>
2019-02-15 10:47:56 +0800

    change file in view laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel branchone
                 </div>
 
                 <div class="links">
